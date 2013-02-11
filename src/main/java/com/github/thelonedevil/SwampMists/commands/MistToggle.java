505ca01826c1a93b1ba98839b0bb84b19d8f521c
Justin Wiblin <jwiblin95@gmail.com>
2013-02-11 23:33:59 +0000

    added logger and message to commands

--- src/main/java/com/github/thelonedevil/SwampMists/commands/MistToggle.java
+++ src/main/java/com/github/thelonedevil/SwampMists/commands/MistToggle.java
@@ -1,4 +1,5 @@
 package com.github.thelonedevil.SwampMists.commands;
+import org.spout.api.Spout;
 import org.spout.api.command.CommandContext;
 import org.spout.api.command.CommandSource;
 import org.spout.api.command.annotated.Command;
@@ -14,10 +15,16 @@ public class MistToggle {
     @CommandPermissions("swampmist.enable")
     public void enable(CommandContext args, CommandSource source) throws CommandException {
         SwampMists.Mist = true;
+        source.sendMessage("Mists have been enabled in swamps");
+        String name = source.getName();
+        Spout.getLogger().info("Mists have been enabled in swamps by "+name);
     }
     @Command(aliases = {"disable", "off", "deactivate", "Let it be clear"}, desc = "turns mist off in swamps")
     @CommandPermissions("swampmist.disable")
     public void disable(CommandContext args, CommandSource source) throws CommandException {
     	SwampMists.Mist = false;
+        source.sendMessage("Mists have been disabled in swamps");
+        String name = source.getName();
+        Spout.getLogger().info("Mists have been disabled in swamps by "+name);
     }
 }
